5951ec5123b2958d5148630eeafca07834ce85b1
Mathieu LALLEMAND <lallemand.mathieu@gmail.com>
2014-02-21 22:00:53 +0100

    Amelioration du client Synode JS

--- synode-server/assets/synode.js
+++ synode-server/assets/synode.js
@@ -1,38 +1,68 @@
+/**
+ * Synode Javascript Client Object
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Mathieu LALLEMAND (@lalmat) - 2014
+ * MIT Licence
+ */
 function synode(host, hash) {
 
   this.bridgeHost = host;
   this.socket     = null;
+  this.connected  = false;
+  this.debug      = true;
 
-  this.isConnected = false;
+  /**
+   * Start the realtime communication with Synode Bridge.
+   * @param  {[function]} onPush
+   * @param  {[function]} onSync
+   * @return {[void]}
+   */
+  this.start = function(onPush, onSync) {
+  	var that = this;
 
-  this.pushCallback = null;
-  this.syncCallback = null;
-
-  this.start = function foo() {
+		if (this.debug) console.log("Synode: Connecting '"+this.bridgeHost+"'");
 		this.socket = io.connect(this.bridgeHost);
 
+		// Handshake method
 		this.socket.on('connect', function() {
-			this.emit("subscribe", {userHash:hash});
+			if (that.debug) console.log("Synode: Socket connected, subscribe using ["+hash+"]");
+			that.socket.emit("subscribe", {userHash:hash});
 	  });
 
-		this.socket.on('push', function(data) {
-			console.log("Push Received :"+data);
-			//if (this.pushCallback != null) this.pushCallback(data);
+		// Server Push : Message from API Server
+		this.socket.on('push', function(msg) {
+			if (that.debug) { console.log("Synode: Server Push Received :"); console.log(msg); }
+			onPush(msg.action, msg.type, msg.data);
 		});
 
-		this.socket.on('sync', function(data) {
-			console.log("Sync Received :"+data);
-			//if (this.syncCallback != null) this.syncCallback(data);
+		// Client Sync : Message from Synode User in the same room
+		this.socket.on('sync', function(msg) {
+			if (that.debug) { console.log("Synode: Client Sync Received :"); console.log(msg); }
+			onSync(msg.action, msg.type, msg.data, msg.uid);
 		});
 
+		// Result of Handshaking
 		this.socket.on("subscribe", function(data) {
-			isConnected = data.result;
-			console.log("isConnected : "+isConnected);
+			that.connected = data.result;
+			console.log("Synode: Webpage connection : "+that.connected);
 		});
 
+		// Connexion reset.
 		this.socket.on("disconnect", function(data) {
-			console.log("Socket closed");
-			this = null;
+			console.log("Synode: Socket closed");
+			that.socket.disconnect();
+			that.socket = null;
+			// Reconnecting
+			setTimeout(function(){that.start(onPush, onSync)},2000);
 		});
+	};
+
+	/**
+	 * Broadcast data to all Synode sockets in the same room as this socket
+   * @param  {[object]} data
+	 */
+	this.sync(data) {
+		if (this.connected) this.socket.emit("sync",data);
+		else console.log("SynodeError: Not connected.");
 	}
 }
---
+++ synode-server/config.inc.php
@@ -0,0 +1,4 @@
+<?php
+// Paramètres de connexion au bridge
+const SYN_HOST   = "http://localhost:1337";
+const SYN_SECRET = "mauvaisMotDePasse";
