d6ed37a8b82ccf5766326848fff285fca3630d12
Mathieu LALLEMAND <mathieu.lallemand@nci.fr>
2014-06-20 11:08:21 +0200

    Renommage du Bridge

--- nRelay-bridge/nRelay.js
+++ nRelay-bridge/nRelay.js
@@ -1,5 +1,5 @@
 /**
- * SYNODE BRIDGE - Run on nodeJS
+ * nRelay BRIDGE - Run on nodeJS
  */
 
 // ----------------------------------------------------------------------------
@@ -7,44 +7,44 @@
 
 var authoritySecret = "mauvaisMotDePasse";    // API password
 
-var debug     = true;  // Enable / disable debug mode
-var port      = 1337;  // Listening port
+var debug     = true;           // Enable / disable debug mode
+var port      = 1337;           // Listening port
 var userAry   = new Array();    // Array of users tokens allowed for connexion
 var roomCount = new Array();    // Array of rooms with number of user connected to.
-var gcTimeout = 5;     // Garbage Collector Interval (in sec.)
-var cxTimeout = 30;    // Max duration allowed to connect (in sec.)
+var gcTimeout = 5;              // Garbage Collector Interval (in sec.)
+var cxTimeout = 30;             // Max duration allowed to connect (in sec.)
 
 
 //-----------------------------------------------------------------------------
-// Serveur Synode
+// Serveur nRelay
 
 var jsSHA = require("jssha");
 var io    = require("socket.io").listen(port, { log: false });
 
-if (debug) console.log("[SYNODE] Brige listening on port "+port);
+if (debug) console.log("[nRelay] Brige listening on port "+port);
 
 io.sockets.on('connection', function (socket) {
 
   socket.on('disconnect', function() {
-    if (debug) console.log("[SYNODE] Socket disconnected");
+    if (debug) console.log("[nRelay] Socket disconnected");
     if (socket.isUser) {
-      if (debug) console.log("[SYNODE] Socket of user "+socket.uid+" removed from "+socket.room);
+      if (debug) console.log("[nRelay] Socket of user "+socket.uid+" removed from "+socket.room);
       roomCount[socket.room]--;
     }
   });
 
   socket.on('allow', function(order) {
-    if (debug) console.log("[SYNODE] Incomming allow...");
+    if (debug) console.log("[nRelay] Incomming allow...");
     if(isAutority(order.tokenSalt, order.tokenTest)) {
       userAry[order.userHash] = { uid:order.userId, allowed:order.access, room:order.room, expire:(getTimestamp()+30) };
-      if (debug) console.log("[SYNODE] "+order.userHash+" allowed to join "+userAry[order.userHash].room+" until "+userAry[order.userHash].expire);
+      if (debug) console.log("[nRelay] "+order.userHash+" allowed to join "+userAry[order.userHash].room+" until "+userAry[order.userHash].expire);
     } else {
-      if (debug) console.log("[SYNODE] Rejecting authority : "+order.tokenTest+" failed.");
+      if (debug) console.log("[nRelay] Rejecting authority : "+order.tokenTest+" failed.");
     }
   });
 
   socket.on('subscribe', function(data) {
-    if (debug) console.log("[SYNODE] Incomming subscription");
+    if (debug) console.log("[nRelay] Incomming subscription");
     if (userAry[data.userHash] != null && userAry[data.userHash].allowed) {
       socket.uid    = userAry[data.userHash].uid;
       socket.room   = userAry[data.userHash].room;
@@ -52,37 +52,37 @@ io.sockets.on('connection', function (socket) {
       socket.join(socket.room);
       roomCount[socket.room] = (roomCount[socket.room] > 0) ? roomCount[userAry[data.userHash].room]++ : 1;
       userAry[data.userHash].allowed = false; // Prevent multiple connexions.
-      if (debug) console.log("[SYNODE] User "+socket.uid+" allowed in "+socket.room+" ("+roomCount[socket.room]+" users).");
+      if (debug) console.log("[nRelay] User "+socket.uid+" allowed in "+socket.room+" ("+roomCount[socket.room]+" users).");
       socket.emit("subscribe", {result:true});
     }
     else {
       socket.emit("subscribe", {result:false});
-      if (debug) console.log("[SYNODE] Incomming user denied.");
+      if (debug) console.log("[nRelay] Incomming user denied.");
       socket.disconnect();
     }
   });
 
   socket.on('push', function(order){
-    if (debug) console.log("[SYNODE] Incomming Server Push ("+order.userId+" => "+order.room+"):");
+    if (debug) console.log("[nRelay] Incomming Server Push ("+order.userId+" => "+order.room+"):");
     if(isAutority(order.tokenSalt, order.tokenTest)) {
       //if (debug) console.log(order.data);
       io.sockets.in(order.room).emit("push", {uid:order.userId, action:order.action, data:order.data});
     } else {
-      if (debug) console.log("[SYNODE] Server Push not allowed : bad password");
+      if (debug) console.log("[nRelay] Server Push not allowed : bad password");
     }
   });
 
   socket.on('sync', function(syncData){
-    if (debug) console.log("[SYNODE] Incomming Client Sync ("+socket.uid+" => "+socket.room+"):")
+    if (debug) console.log("[nRelay] Incomming Client Sync ("+socket.uid+" => "+socket.room+"):")
     if (socket.isUser) {
       //if (debug) console.log(syncData);
       io.sockets.in(socket.room).emit("sync", {uid:socket.uid, action:syncData.action, data:syncData.data});
     } else {
-      if (debug) console.log("[SYNODE] Sync not allowed : not a user");
+      if (debug) console.log("[nRelay] Sync not allowed : not a user");
     }
   });
 
-  if (debug) console.log("[SYNODE] New connexion !");
+  if (debug) console.log("[nRelay] New connexion !");
 });
 
 setInterval(garbageCollect, gcTimeout*1000);
@@ -114,7 +114,7 @@ function garbageCollect() {
     if (userAry[userHash].expire-curTimestamp > 0 && userAry[userHash].allowed) {
       tmpUserAry[userHash] = userAry[userHash];
     } else {
-      if (debug) console.log("[SYNODE] Removing hash:"+userHash);
+      if (debug) console.log("[nRelay] Removing hash:"+userHash);
     } 
   }
   userAry = tmpUserAry;
@@ -125,7 +125,7 @@ function garbageCollect() {
     if (roomCount[room]>0) {
       tmpRoomCount[room] = roomCount[room];
     } else {
-      if (debug) console.log("[SYNODE] Removing room:"+room);
+      if (debug) console.log("[nRelay] Removing room:"+room);
     }
   }
   roomCount = tmpRoomCount;
--- synode-server/config.inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-// SYNODE SERVER - Bridge Configuration.
-const SYN_HOST   = "http://192.168.79.45:1337";
-const SYN_SECRET = "mauvaisMotDePasse";
