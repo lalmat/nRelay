465a5511f7c6742bb5f9433c1283421e85fd8fca
Mathieu LALLEMAND <lallemand.mathieu@gmail.com>
2014-09-14 14:59:15 +0200

    Splitting projects between server and PHP client

--- nRelay-server/config.inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-// nRelay Server - Bridge Configuration.
-const NRLY_HOST   = "http://localhost:1337";
-const NRLY_SECRET = "mauvaisMotDePasse";
--- nRelay-server/vendors/Elephant.IO/README.txt
+++
@@ -1,1 +0,0 @@
-This is a forked and personalized version of Elephant.IO
